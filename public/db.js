94e8949fc20e63d77523db691ae1b096329bef3b
m-llo <markalloyd21@gmail.com>
2021-05-06 21:18:46 -0500

    updating service worker cached files

--- public/db.js
+++ public/db.js
@@ -1,7 +1,5 @@
 let db;
-// let budgetVersion;
 
-// const request = indexedDB.open('BudgetDB', budgetVersion || 1);
 const request = indexedDB.open('BudgetDB', 1);
 
 request.onupgradeneeded = function (event) {
@@ -26,18 +24,17 @@ request.onupgradeneeded = function (event) {
 function checkDatabase() {
   console.log('check db invoked');
 
-  // Open a transaction on your BudgetStore db
+  
   let transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-  // access your BudgetStore object
   const store = transaction.objectStore('BudgetStore');
 
-  // Get all records from store and set to a variable
+  
   const getAll = store.getAll();
 
-  // If the request was successful
+
   getAll.onsuccess = function () {
-    // If there are items in the store, we need to bulk add them when we are back online
+   
     if (getAll.result.length > 0) {
       fetch('/api/transaction/bulk', {
         method: 'POST',
@@ -49,15 +46,15 @@ function checkDatabase() {
       })
         .then((response) => response.json())
         .then((res) => {
-          // If our returned response is not empty
+         
           if (res.length !== 0) {
-            // Open another transaction to BudgetStore with the ability to read and write
+          
             transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-            // Assign the current store to a variable
+            
             const currentStore = transaction.objectStore('BudgetStore');
 
-            // Clear existing entries because our bulk add was successful
+           
             currentStore.clear();
             console.log('Clearing store');
           }
@@ -70,7 +67,7 @@ request.onsuccess = function (event) {
   console.log('success');
   db = event.target.result;
 
-  // Check if app is online before reading from db
+  
   if (navigator.onLine) {
     console.log('Backend online! 🗄️');
     checkDatabase();
@@ -79,15 +76,15 @@ request.onsuccess = function (event) {
 
 const saveRecord = (record) => {
   console.log('Save record invoked');
-  // Create a transaction on the BudgetStore db with readwrite access
+  
   const transaction = db.transaction(['BudgetStore'], 'readwrite');
 
-  // Access your BudgetStore object store
+  
   const store = transaction.objectStore('BudgetStore');
 
-  // Add record to your store with add method.
+
   store.add(record);
 };
 
-// Listen for app coming back online
+
 window.addEventListener('online', checkDatabase);
